7b351acfc5b8aabfdc34361c4b1cf7d5d72697e4
FTDalekTroyd <dalektroyd@gmail.com>
2026-01-14 15:49:12 +0000

    jave bs

---
+++ codes.js
@@ -0,0 +1,15 @@
+
+
+let title = document.querySelector("h1")
+
+title.innerHTML = "hi"
+
+console.log("hello world")
+
+alert ("fuck you")
+
+let userName = prompt("WHAT IS YOUR NAME📢")
+
+let output = "Fuck you  " + userName + "🦊";
+
+title.innerHTML = output;
